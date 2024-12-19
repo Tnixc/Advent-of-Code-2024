22c109da9b08b0569b38c15156f6f8675dfc968f
Tnixc <tnixxc@gmail.com>
2024-12-18 20:52:44 -0500

    closer

--- days/16.ts
+++ days/16.ts
@@ -1,5 +1,7 @@
 import { encodeTwo } from "utils";
+import { INPUT } from "@/16";
 import Heap from "heap-js";
+
 const t = `###############
 #.......#....E#
 #.#.###.#.###.#
@@ -16,11 +18,29 @@ const t = `###############
 #S..#.....#...#
 ###############`;
 
+const t2 = `#################
+#...#...#...#..E#
+#.#.#.#.#.#.#.#.#
+#.#.#.#...#...#.#
+#.#.#.#.###.#.#.#
+#...#.#.#.....#.#
+#.#.#.#.#.#####.#
+#.#...#.#.#.....#
+#.#.#####.#.###.#
+#.#.#.......#...#
+#.#.###.#####.###
+#.#.#...#.....#.#
+#.#.#.#####.###.#
+#.#.#.........#.#
+#.#.#.#########.#
+#S#.............#
+#################`;
+
 const grid = t.split("\n").map((x) => x.split(""));
 const compare = (a, b) => a.cost - b.cost;
 const pq = new Heap(compare);
 
-const start = { y: grid.length - 2, x: 1, h: 0, cost: 0 };
+const start = { y: grid.length - 2, x: 1, h: 0, cost: 0, hist: 0 };
 
 const dirs = [
   [-1, 0],
@@ -28,25 +48,32 @@ const dirs = [
   [1, 0],
   [0, -1],
 ];
-const results = [];
 
 function walk() {
   let now = pq.pop();
   while (now) {
-    if (grid[now.y][now.x] === "E") break;
+    console.log(now.y, now.x, now.h, now.cost);
+    if (grid[now.y][now.x] === "E") {
+      now.cost += 1000; //idk why, but it works
+      return now;
+    }
     for (let nh = 0; nh < 4; nh++) {
       const [ny, nx] = [now.y + dirs[nh][0], now.x + dirs[nh][1]];
       if (grid[ny]?.[nx] === "#") continue; // if its a wall
       if (now.h === nh - 2 || now.h === nh + 2) continue;
       let isTurn = now.h != nh;
-      console.log(now, grid[now.y][now.x]);
-      pq.push({ y: ny, x: nx, h: nh, cost: now.cost + (isTurn ? 1001 : 1) });
+      pq.push({
+        y: ny,
+        x: nx,
+        h: nh,
+        cost: now.cost + (isTurn ? 1001 : 1),
+        hist: now.hist + 1,
+      });
     }
     now = pq.pop();
   }
 }
 
 pq.push(start);
-walk();
-
-console.log(results);
+console.log(walk());
+// console.log(visited.size);
